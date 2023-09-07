a946f66eda208eb18332f1533115370adb52244a
Dao Thi Phuong Lan <lt@gmail.com>
2023-09-08 00:46:59 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+import {Routes,Route} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Store } from './pages/Store'
+import { Navbar } from './components/Navbar'
+import { ShoppingCartProvider } from './context/ShoppingCartContext'
+function App() {
+  
+
+  return (
+    <>
+    
+     <ShoppingCartProvider >
+     <Navbar/>
+
+<Container>
+   <Routes>
+     <Route path="/" element={<Home></Home>}/>
+     <Route path="/about" element={<About></About>}/>
+     <Route path="/store" element={<Store></Store>}/>
+   </Routes>
+</Container>
+
+     </ShoppingCartProvider>
+      
+     
+    </>
+  )
+}
+
+export default App
